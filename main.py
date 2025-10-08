e1bcc9f49b10180e1fabe4761f0b45ac86f38aae
Isabelle Kuder <isabelle.kuder@gmail.com>
2025-10-08 22:31:16 +0100

    set dest LHR as condition

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def find_closest_flight(fr_api, lat, lon, radius, max_altitude):
                 flight.latitude
                 and flight.longitude
                 and flight.number
+                and flight.destination_airport_iata == "LHR"
                 and flight.altitude < max_altitude
             ):
                 dist = haversine(lat, lon, flight.latitude, flight.longitude)
