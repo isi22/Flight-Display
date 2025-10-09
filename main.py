472920068525a92759b5ef268bfbc6a71358827d
Isabelle Kuder <isabelle.kuder@gmail.com>
2025-10-09 17:47:14 +0100

    Increased API timeout

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def main():
     SEARCH_RADIUS_KM = 3  # Search within a 20km radius
     MAX_ALTITUDE_FT = 5000  # Only consider flights below this altitude
     REFRESH_INTERVAL_SECONDS = 5
-    API_TIMEOUT = 1
+    API_TIMEOUT = 2
 
     display = get_display()
     display.start()
