bf29e57ed65452d8aef74f38cf3795688975d238
Isabelle Kuder <isabelle.kuder@gmail.com>
2025-10-14 11:50:10 +0100

    debug

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ def main():
     # Define your home location and search radius in kilometers
     HOME_LAT = 51.487077
     HOME_LON = -0.217605
-    SEARCH_RADIUS_KM = 3  # Search within a 20km radius
-    MAX_ALTITUDE_FT = 5000  # Only consider flights below this altitude
+    SEARCH_RADIUS_KM = 15  # Search within a 20km radius
+    MAX_ALTITUDE_FT = 30000  # Only consider flights below this altitude
     REFRESH_INTERVAL_SECONDS = 5
     API_TIMEOUT = 2
 
